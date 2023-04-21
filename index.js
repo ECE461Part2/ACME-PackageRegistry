3a9881a4bbeadc0a9f859439afa484a3576d85cd
Hugo Day <day98@purdue.edu>
2023-04-21 18:10:41 -0400

    Delete files from bucket

--- index.js
+++ index.js
@@ -752,6 +752,14 @@ app.delete("/reset", auth, (req, res) => {
     return;
   }
 
+  bucket.deleteFiles()
+  .then(() => {
+    console.log(`All files deleted from bucket ${bucketName}.`)
+  })
+  .catch((err) => {
+    error(res, err)
+  })
+
   db.run('DELETE FROM users; DELETE FROM packages;', function(err) {
     if (err) {
       error(res, err)
